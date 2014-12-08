8f645762684f391107dbdb2be7b29a0b2d8104ad
Hrvoje Grabušić <hrvoje.grabusic@gmail.com>
2014-12-08 01:19:29 +0100

    multi crop model

--- Kooboo.CMS/Kooboo.CMS.Web/Areas/Contents/Models/CropInfo.cs
+++ Kooboo.CMS/Kooboo.CMS.Web/Areas/Contents/Models/CropInfo.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Kooboo.CMS.Web.Areas.Contents.Models
 {
     public class CropInfo : Dictionary<string,CropBox>
     {
         public CropInfo() { }
+
+        public CropBox GetPosition(string key)
+        {
+            if(this.ContainsKey(key))
+            {
+                return this[key];
+            } else{
+                CropBox cb = new CropBox();
+                this.Add(key, cb);
+                return cb;
+            }
+        }
     }
     public class CropBox
     {
@@ -15,5 +28,13 @@ namespace Kooboo.CMS.Web.Areas.Contents.Models
         public int y = 0;
         public int width = 0;
         public int height = 0;
+
+        public void BindValues(FormCollection form)
+        {
+            x = int.Parse(form["x"]);
+            y = int.Parse(form["y"]);
+            width = int.Parse(form["width"]);
+            height = int.Parse(form["height"]);
+        }
     }
 }
